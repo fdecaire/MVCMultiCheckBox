8995ab3bb0361995b9bdbe02f344051a7972ccbb
fdecaire <frank@decaire.net>
2015-07-19 11:39:12 -0400

    Added Files

---
+++ MVCMultiCheckBox/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMultiCheckBox.Models;
+
+namespace MVCMultiCheckBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+		public ActionResult Index()
+		{
+			var myHomeViewModel = new HomeViewModel();
+
+			myHomeViewModel.Persons = new List<PersonModel>();
+			myHomeViewModel.Persons.Add(new PersonModel { Name = "Frank", Checked = false });
+			myHomeViewModel.Persons.Add(new PersonModel { Name = "Joe", Checked = false });
+			myHomeViewModel.Persons.Add(new PersonModel { Name = "Sue", Checked = false });
+			myHomeViewModel.Persons.Add(new PersonModel { Name = "Mike", Checked = false });
+
+			return View(myHomeViewModel);
+		}
+
+		[HttpPost]
+		public ActionResult Submit(HomeViewModel result)
+		{
+			return View("Index", result);
+		}
+    }
+}
---
+++ MVCMultiCheckBox/Models/HomeViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMultiCheckBox.Models
+{
+	public class HomeViewModel
+	{
+		public List<PersonModel> Persons { get; set; }
+	}
+
+	public class PersonModel
+	{
+		public string Name { get; set; }
+		public bool Checked { get; set; }
+	}
+}
